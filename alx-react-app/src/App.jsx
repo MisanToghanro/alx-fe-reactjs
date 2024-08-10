53a981c24671610cf2de10f99736d311187ff45a
MisanToghanro <tmisan657@gmail.com>
2024-08-10 08:57:44 +0100

    add react components

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,20 @@
+
+import WelcomeMessage from './WelcomeMessage';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+import UserProfile from './UserProfile';
+
+function App() {
+return(
+    <div>
+         <WelcomeMessage /> 
+         <Header/>
+         <MainContent/>
+         <Footer/>
+         <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    </div>
+);
+}
+
+export default App
